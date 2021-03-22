49528c91815887bd9a524875c9a04529a63c78ba
Shaheer Shaikh <shayana803@gmail.com>
2021-03-22 23:05:04 +0500

    minor change

--- backend/utils.js
+++ backend/utils.js
@@ -7,7 +7,7 @@ export const generateToken = (user) => {
         email: user.email,
         isAdmin: user.isAdmin,
     },
-        process.env.JWT_SECRET,
+        process.env.JWT_SECRET || 'somethingsercret',
     {
         expiresIn: '30d',
     });
